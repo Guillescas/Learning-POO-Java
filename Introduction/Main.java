e0f6114541ceab4aaa6c33e1a3ee5c605e44a4fa
Guilherme Illescas <gui.illescas@gmail.com>
2020-10-26 23:06:06 -0300

    Calling constructor method

--- Introduction/Main.java
+++ Introduction/Main.java
@@ -2,15 +2,8 @@ package Introduction;
 
 public class Main {
   public static void main(String[] args) {
-    Caneta c1 = new Caneta();
+    Caneta c1 = new Caneta("BIC", "Vermelha", 0.5f);
 
-    c1.modelo = "Bic";
-    c1.cor = "Azul";
-    // c1.ponta = 0.5f;
-    c1.carga = 80;
-    c1.tampada = false;
-
-    c1.destampar();
     c1.status();
     c1.rabiscar();
   }
