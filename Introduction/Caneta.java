701aa769ba7b802d49e62a1e487b75fb9ac612d2
Guilherme Illescas <gui.illescas@gmail.com>
2020-10-26 23:05:48 -0300

    Implement constructor method and getters & setters

--- Introduction/Caneta.java
+++ Introduction/Caneta.java
@@ -8,9 +8,10 @@ public class Caneta {
   protected boolean tampada;
 
   public void status() {
-    System.out.println("Modelo: " + this.modelo);
+    System.out.println("==== Sobre a caneta ====");
+    System.out.println("Modelo: " + this.getModelo());
     System.out.println("Caneta na cor " + this.cor);
-    System.out.println("Pomta: " + this.ponta);
+    System.out.println("Pomta: " + this.getPonta());
     System.out.println("Carga: " + this.carga);
     System.out.println("Tampada? " + this.tampada);
   }
@@ -23,11 +24,51 @@ public class Caneta {
     }
   }
 
-  protected void tampar() {
-    this.tampada = true;
-  }
+ public int getCarga() {
+   return carga;
+ }
 
-  protected void destampar() {
-    this.tampada = false;
+ public void setCarga(int carga) {
+   this.carga = carga;
+ }
+
+ public String getCor() {
+   return cor;
+ }
+
+ public void setCor(String cor) {
+   this.cor = cor;
+ }
+
+ public String getModelo() {
+   return modelo;
+ }
+
+ public void setModelo(String modelo) {
+   this.modelo = modelo;
+ }
+
+ public float getPonta() {
+   return ponta;
+ }
+
+ public void setPonta(float ponta) {
+   this.ponta = ponta;
+ }
+
+ public boolean isTampada() {
+   return tampada;
+ }
+
+ public void setTampada(boolean tampada) {
+   this.tampada = tampada;
+ }
+
+  public Caneta(String modelo, String cor, float ponta) {
+    this.modelo = modelo;
+    this.cor = cor;
+    this.ponta = ponta;
+    this.setTampada(true);
+    this.cor = "Azul";
   }
 }
