0a9265cc511f2a2714c4113208e2c2417a2319c3
Guilherme Illescas <gui.illescas@gmail.com>
2020-10-27 13:16:58 -0300

    Create interface for Remote Control

---
+++ RemoteControl/IRemoteControl.java
@@ -0,0 +1,14 @@
+package RemoteControl;
+
+interface IRemoteControl {
+  public abstract void turnOn();
+  public abstract void turnOff();
+  public abstract void openMenu();
+  public abstract void closeMenu();
+  public abstract void volumeUp();
+  public abstract void volumeDown();
+  public abstract void mute();
+  public abstract void unmute();
+  public abstract void play();
+  public abstract void pause();
+}
